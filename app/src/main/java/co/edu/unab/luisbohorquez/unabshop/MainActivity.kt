df607d611900884749d82da6f6b241f7c833d781
luis bohorquez <lbohorquez662@unab.edu.co>
2025-10-15 09:50:26 -0500

    setup Navigation

--- app/src/main/java/co/edu/unab/luisbohorquez/unabshop/MainActivity.kt
+++ app/src/main/java/co/edu/unab/luisbohorquez/unabshop/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import co.edu.unab.luisbohorquez.unabshop.ui.theme.UnabShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UnabShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+            val navController = rememberNavController()
+            val startDestination =" login "
+
+            NavHost(navController, startDestination) {
+                composable( route="login") {
+                    LoginScreen()
                 }
+                composable(route="register") {
+                    RegisterScreen()
+                }
+                composable(route="home") {
+                    HomeScreen()
+                }
+
             }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnabShopTheme {
-        Greeting("Android")
-    }
-}
